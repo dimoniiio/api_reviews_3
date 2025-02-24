38c7bf477f0ead6c14a0c2e0490896859b385187
Dmitriy Ivanov <dimoniiio@yandex.ru>
2025-02-24 17:04:13 +0300

    add MyUser

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class MyUser(AbstractUser):
+    """Класс описывающий пользователя."""
+    ROLE_CHOICES = [
+        ('anon', 'Аноним'),
+        ('admin', 'Администратор'),
+        ('moderator', 'Модератор'),
+        ('user', 'Пользователь'),
+    ]
+
+    role = models.CharField(
+        'Роль',
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+    bio = models.TextField('Биография', blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
