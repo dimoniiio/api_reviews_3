e587421d12a298612e480a5cbfc2983c0473f0e6
Dmitriy Ivanov <dimoniiio@yandex.ru>
2025-02-26 20:36:09 +0300

    Создал view для добавления новых пользователей админом Переписал запросы на адрес ...users/me

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import AuthViewSet, CategoryViewSet, GenreViewSet, TitleViewSet
+from .views import (AuthViewSet, CategoryViewSet, GenreViewSet, TitleViewSet,
+                    UserViewSet)
 
 app_name = 'api'
 
 v1_router = DefaultRouter()
 v1_router.register('auth', AuthViewSet, basename='auth')
+v1_router.register(r'users', UserViewSet, basename='users')
 v1_router.register('titles', TitleViewSet, basename='titles')
 v1_router.register('categories', CategoryViewSet, basename='categories')
 v1_router.register('genres', GenreViewSet, basename='genres')
