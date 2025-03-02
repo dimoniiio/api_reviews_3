a32bdec94b7156bcf37d60e0d7fa89e82ad3e85b
Dmitriy <99862867+dimoniiio@users.noreply.github.com>
2025-03-02 11:17:59 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,4 @@
-import datetime
-
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
